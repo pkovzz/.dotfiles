549c99f9943f40845d2ce635eae043950138967b
Péter Kovács <kov2peter@gmail.com>
2022-11-16 16:06:11 +0100

    add cmp

--- lua/pkovzz/packer.lua
+++ lua/pkovzz/packer.lua
@@ -13,9 +13,23 @@ return require('packer').startup(function(use)
   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
  }
 
- use 'nvim-lualine/lualine.nvim' -- Statusline
- 
  -- For file navigation
  use "nvim-lua/plenary.nvim"
  use "nvim-telescope/telescope.nvim"
+
+ use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+
+ use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+ use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+ -- cmp plugins
+ use "hrsh7th/nvim-cmp" -- The completion plugin
+ use "hrsh7th/cmp-buffer" -- buffer completions
+ use "hrsh7th/cmp-path" -- path completions
+ use "hrsh7th/cmp-cmdline" -- cmdline completions
+ use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+ -- snippets
+ use "L3MON4D3/LuaSnip" --snippet engine
+ use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 end)
